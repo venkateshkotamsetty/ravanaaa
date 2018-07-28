00645b5283c56e52d64c4167988c4a0181c8c84f
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-07-28 18:46:06 +0530

    Create ravaa.java

---
+++ ravaa.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner (System.in);
+		int num = s.nextInt();
+		 while (num > 0)
+		 {
+		 	System.out.println("Hello");
+		 	num=num-1;
+		 }
+		
+	}
+}
